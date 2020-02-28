88d963e6daed5556980e661429562653ba989b48
ricardo-ljr <ricardo.leitejr95@gmail.com>
2020-02-28 10:18:52 -0700

    Completed MVP

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,25 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import BubblePage from "./components/BubblePage";
 
 import Login from "./components/Login";
 import "./styles.scss";
+import PrivateRoute from "./utils/PrivateRoute";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
+        <Switch>
+          <PrivateRoute path="/protected" component={BubblePage} />
+          <Route exact path="/" component={Login} />
+          <Route component={Login} />
+
+          {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+        </Switch>
       </div>
     </Router>
   );
--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -21,10 +22,33 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+      .put(`/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res => {
+        console.log(res);
+        console.log(colorToEdit);
+        updateColors([
+          ...colors.filter(color => color.id !== colorToEdit.id),
+          res.data
+        ]);
+        setEditing(false);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth()
+      .delete(`/colors/${color.id}`)
+      .then(res => {
+        console.log(res);
+        updateColors(colors.filter(color => color.id !== res.data));
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
 
   return (
@@ -34,12 +58,14 @@ const ColorList = ({ colors, updateColors }) => {
         {colors.map(color => (
           <li key={color.color} onClick={() => editColor(color)}>
             <span>
-              <span className="delete" onClick={e => {
-                    e.stopPropagation();
-                    deleteColor(color)
-                  }
-                }>
-                  x
+              <span
+                className="delete"
+                onClick={e => {
+                  e.stopPropagation();
+                  deleteColor(color);
+                }}
+              >
+                x
               </span>{" "}
               {color.color}
             </span>
